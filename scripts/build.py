1a4b07a2a5a4ae61ccd63f322771932434e5c341
Atsushi Kokubo <kokubo@hi-tech.ac.jp>
2023-07-16 23:22:35 +0900

    表示の改善

--- scripts/build.py
+++ scripts/build.py
@@ -1,3 +1,4 @@
+import math
 import pandas as pd
 from jinja2 import Template, Environment, FileSystemLoader
 
@@ -5,9 +6,16 @@ env = Environment(loader=FileSystemLoader('./', encoding='utf8'))
 tmpl = env.get_template('templates/index.html')
 
 df = pd.read_excel('data/haga.xlsx')
-df = df.fillna('')
+df['進捗'] = df['進捗'].fillna(-1)
+df['進捗'] = df['進捗'].astype(int)
+progress_max = df['進捗'].max()
 for index, row in df.iterrows():
   record = dict()
+  if row['進捗'] > 1:
+    record['progress'] = round(row['進捗'] / progress_max * 100)
+  else:
+    record['progress'] = None
+
   record['current'] = row['ファイル名']
   record['next'] = row['次のファイル名']
   record['type'] = row['type']
@@ -16,11 +24,16 @@ for index, row in df.iterrows():
     record['full_title'] = record['name']
   else:
     record['full_title'] = record['name'] + ' | 集団健康度チェック'
-  record['header'] = row['ヘッダー']
-  if len(row['選択肢']) > 0:
-    record['choices'] = row['選択肢'].split(',')
+
+  if type(row['ヘッダー']) is str:
+    record['header'] = row['ヘッダー']
   else:
+    record['header'] = ''
+
+  if pd.isna(row['選択肢']):
     record['choices'] = ''
+  else:
+    record['choices'] = row['選択肢'].split(',')
 
   rendered = tmpl.render(record)
 
